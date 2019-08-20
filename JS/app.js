6918b4afe52b9186072fbf1ede0773f7df90c828
didishas <didier.ukanda@gmail.com>
2019-08-20 12:00:09 -0300

    computerPlay func returns randomly rock,scissors or paper created AND playRound func returns winner Results created

--- JS/app.js
+++ JS/app.js
@@ -1,10 +1,47 @@
 console.log('Starting a New Game');
-// Constants and Variables
-const signs = ['Rock', 'Paper', 'Scissors'];
+
+const Controller = (function(){
+    // Constants and Variables
+const signs = ['rock', 'paper', 'scissors'];
 
 //todo 3. Create  a function called computerPlay that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’ 👍
 // My Principal Functions 
-const computerPlay = function(){
-    return signs[Math.floor(Math.random()*signs.length)]
+return {
+    rocks: signs[0],
+    paper: signs[1],
+    scissors: signs[2],
+    computerPlay(){
+        return signs[Math.floor(Math.random()*signs.length)]
+    },
+    // todo 4. Write a function that plays a single round of Rock Paper Scissors then return a string that declares the winner of the round 👍
+    playRound(playerSelection, computerSelection) {
+        // your code here!
+        playerSelection = playerSelection.toLowerCase();
+        // let playerSelection = prompt("What is your choice ?");
+        if((playerSelection === this.rocks)  && (computerSelection === this.scissors)){
+            return 'You Win! Rock beats Scissors';
+        }else if((computerSelection === this.rocks)  && (playerSelection === this.scissors)){
+            return 'You Lose! Rock beats Scissors';
+        }else if((playerSelection === this.paper)  && (computerSelection === this.scissors)){
+            return 'You Lose! Scissors beats Paper';
+        }else if((computerSelection === this.paper)  && (playerSelection === this.scissors)){
+            return 'You Win! Scissors beats Paper';
+        }else if((playerSelection === this.rocks)  && (computerSelection === this.paper)){
+            return 'You Lose! Paper beats Rock';
+        }else if((computerSelection === this.rocks)  && (playerSelection === this.paper)){
+            return 'You Win! Paper beats Rock';
+        }else {
+            return 'equality';
+        }
+    }
 }
-computerPlay();
+})();
+
+const UI = (function(){
+    let playerSelection = 'RoCK';
+    let computerSelection = Controller.computerPlay();
+    return Controller.playRound(playerSelection, computerSelection);
+})();
+
+
+console.log(UI);
